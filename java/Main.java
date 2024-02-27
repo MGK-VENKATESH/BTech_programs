ef8cb70f1df0022cc693ca6900114eebebd70baf
MGK-Venkatesh <mgkvenkatesh3@gmail.com>
2024-02-27 17:33:51 +0530

    java

---
+++ java/Main.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+    // Method to insert a new node at the end of the linked list
+    public void append(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    // Method to display the linked list
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LinkedList list = new LinkedList();
+
+        // Inputting elements dynamically
+        System.out.println("Enter elements to append to the linked list (enter -1 to stop):");
+        while (true) {
+            int element = scanner.nextInt();
+            if (element == -1)
+                break;
+            list.append(element);
+        }
+
+        // Displaying the linked list
+        System.out.println("Linked List:");
+        list.display();
+    }
+}
